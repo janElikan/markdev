c487e2986538d534682f56309752b75efb9ca8fb
Elikan <janelikan@proton.me>
2024-06-14 13:18:21 +0300

    build: set up wasi toolchain

--- flake.nix
+++ flake.nix
@@ -1,21 +1,36 @@
 {
   inputs = {
     naersk.url = "github:nix-community/naersk/master";
+    fenix.url = "github:nix-community/fenix";
     nixpkgs.url = "github:NixOS/nixpkgs/nixpkgs-unstable";
     utils.url = "github:numtide/flake-utils";
   };
 
-  outputs = { self, nixpkgs, utils, naersk }:
+  outputs = { self, nixpkgs, utils, fenix, naersk }:
     utils.lib.eachDefaultSystem (system:
       let
         pkgs = import nixpkgs { inherit system; };
-        naersk-lib = pkgs.callPackage naersk { };
+
+        toolchain = with fenix.packages.${system}; combine [
+          minimal.rustc
+          minimal.cargo
+          targets.wasm32-wasi.latest.rust-std
+        ];
+
+        naersk-lib = pkgs.callPackage naersk {
+          cargo = toolchain;
+          rustc = toolchain;
+        };
       in
       {
-        defaultPackage = naersk-lib.buildPackage ./.;
+        defaultPackage = naersk-lib.buildPackage {
+            src = ./.;
+            CARGO_BUILD_TARGET = "wasm32-wasi";
+        };
         devShell = with pkgs; mkShell {
-          buildInputs = [ cargo rustc rustfmt pre-commit rustPackages.clippy ];
+          buildInputs = [ toolchain rustfmt rustPackages.clippy bacon ];
           RUST_SRC_PATH = rustPlatform.rustLibSrc;
+          CARGO_BUILD_TARGET = "wasm32-wasi";
         };
       }
     );
