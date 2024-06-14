6e3a11c6d701df6ba3b1e2b1a6df1b53e1ab0d92
Elikan <janelikan@proton.me>
2024-06-14 14:40:08 +0300

    feat: plugin scaffolding

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,24 @@
-fn main() {
-    println!("Hello, world!");
+use std::collections::BTreeMap;
+
+use zellij_tile::{prelude::Event, register_plugin, ZellijPlugin, shim::report_panic};
+
+#[derive(Default)]
+struct Plugin {
+    config: BTreeMap<String, String>,
 }
+
+impl ZellijPlugin for Plugin {
+    fn load(&mut self, config: BTreeMap<String, String>) {
+        self.config = config;
+    }
+
+    fn update(&mut self, event: Event) -> bool {
+        true
+    }
+
+    fn render(&mut self, rows: usize, cols: usize) {
+        println!("Hello, world!");
+    }
+}
+
+register_plugin!(Plugin);
