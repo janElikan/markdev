e2c3876d61e9a07822d5e5e641bdd06c47b1e661
jan Elikan <janelikan@proton.me>
2024-06-26 18:15:43 +0000

    feat: implement get_tab_groups
    except it doesn't work yet

--- src/obsidian.rs
+++ src/obsidian.rs
@@ -12,13 +12,12 @@ impl Vault {
     pub fn new(path: PathBuf) -> Result<Self, Error> {
         let mut path = path;
         path.push(PathBuf::from_str(".obsidian/workspace.json").unwrap());
-        dbg!(&path);
 
         let workspace_file = fs::File::open(&path)?;
         let nodes: Value = serde_json::from_reader(workspace_file)?;
-        dbg!(&nodes);
+        let main = nodes.get("main").unwrap();
 
-        let tab_groups = get_tab_groups(&nodes);
+        let tab_groups = get_tab_groups(&main);
         let focused_group = nodes.get("active").unwrap().to_string();
 
         Ok(Self {
@@ -38,8 +37,24 @@ impl Vault {
     }
 }
 
-fn get_tab_groups(raw_data: &Value) -> Vec<TabGroup> {
-    todo!()
+/// this should eventually become a const function
+fn get_tab_groups(node: &Value) -> Vec<TabGroup> {
+    if node.is_array() {
+        let node = node.as_array().unwrap();
+        return node.into_iter().flat_map(get_tab_groups).collect();
+    }
+
+    let node_type = node.get("type").unwrap().as_str().unwrap();
+
+    return match node_type {
+        "tabs" => {
+            vec![TabGroup::from(node)]
+        }
+        _ => {
+            let children = node.get("children").unwrap();
+            get_tab_groups(children)
+        }
+    };
 }
 
 struct TabGroup {
@@ -48,9 +63,57 @@ struct TabGroup {
     pub active_tab: usize,
 }
 
+impl From<&Value> for TabGroup {
+    // ## Breaks at runtime
+    // if you have more than 18_446_744_073_709_551_615 tabs open!
+    fn from(value: &Value) -> Self {
+        let id = value.get("id").unwrap().to_string();
+
+        let active_tab = match value.get("currentTab").unwrap().as_number() {
+            Some(number) => number.as_u64().unwrap() as usize,
+            None => 0,
+        };
+
+        let tabs = value
+            .get("children")
+            .unwrap()
+            .as_array()
+            .unwrap()
+            .into_iter()
+            .map(Tab::from)
+            .collect();
+
+        Self {
+            id,
+            tabs,
+            active_tab,
+        }
+    }
+}
+
 pub enum Tab {
     File(File),
     Plugin,
+}
+
+impl From<&Value> for Tab {
+    /// This function currently has uncomfortably many `.unwrap()`s
+    fn from(value: &Value) -> Self {
+        let value = value.get("state").unwrap(); // throw away the leaf ID
+
+        match value.get("type").unwrap().as_str().unwrap() {
+            "markdown" => {
+                let state = value.get("state").unwrap();
+                dbg!(state);
+
+                Tab::File(File {
+                    path: state.get("file").unwrap().as_str().unwrap().into(),
+                    mode: FileMode::Unimplemented,
+                })
+            }
+            _ => Tab::Plugin,
+        }
+    }
 }
 
 pub struct File {
@@ -58,7 +121,9 @@ pub struct File {
     pub mode: FileMode,
 }
 
+/// this is not yet used
 pub enum FileMode {
+    Unimplemented,
     Source,
     Edit,
     Preview,
